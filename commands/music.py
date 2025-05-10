c7b6d0dcad78ee67addc446742757f18f0db7eac
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2025-05-11 00:19:13 +0200

    fix and improve lyrics command

--- commands/music.py
+++ commands/music.py
@@ -5,7 +5,6 @@ from urllib import parse
 import requests
 import textwrap
 
-
 class MusicCommands(app_commands.Group):
     @app_commands.command(name="play", description="Play a song")
     async def play(self, interaction: discord.Interaction):
@@ -16,22 +15,26 @@ class MusicCommands(app_commands.Group):
 
     @app_commands.command(name="lyrics", description="Get the lyrics of a song")
     async def lyrics(self, interaction: discord.Interaction, search: str):
-        r = requests.get("https://some-random-api.com/lyrics?title=" + urllib.parse.quote(search))
+        # Defer at the start to avoid timeout because of slow API response
+        await interaction.response.defer(ephemeral=True, thinking=True)
+        r = requests.get("https://api.some-random-api.com/lyrics?title=" + urllib.parse.quote(search))
         try:
             song_lyrics = r.json()['lyrics']
-            song_author = r.json()['author']
+            song_artist = r.json()['artist']
             song_title = r.json()['title']
-            song_thumbnail = r.json()['thumbnail']['genius']
-            song_link = r.json()['links']['genius']
+            song_thumbnail = r.json()['thumbnail']
+            song_link = r.json()['url']
+
             for chunk in textwrap.wrap(song_lyrics, 4096, replace_whitespace=False):
-                embed = discord.Embed(title=song_title + " by " + song_author, description=chunk,
+                embed = discord.Embed(title=song_title + " by " + song_artist, description=chunk,
                                       colour=discord.Colour(0x00ff00), url=song_link)
                 embed.set_thumbnail(url=song_thumbnail)
-                await interaction.response.send_message(embed=embed)
+                await interaction.followup.send(embed=embed, ephemeral=False)
+
         except KeyError or discord.errors.NotFound:
             embed = discord.Embed(title=":x: Error", colour=discord.Colour(0xff0000),
                                   description=f"Could not find lyrics for `{search}`!")
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.followup.send(embed=embed, ephemeral=True)
 
 
 async def setup(bot):
